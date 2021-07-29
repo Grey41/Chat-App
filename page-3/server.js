22bc59c273443b1a55c73fd8d166f172a7a0e0ef
Grey41 <85015029+Grey41@users.noreply.github.com>
2021-07-29 11:40:04 +0100

    Delete server.js

--- page-3/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const app = express()
-const server = require("http").Server(app)
-const io = require("socket.io")(server)
-
-app.use(express.static(__dirname + "/public"))
-app.get("/", (request, response) => response.render("index.html"))
-
-server.listen(5000, () => console.log("server listening"))
