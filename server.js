33e7d3b6a89fc8f35d72feacb228670c5c4ca1c7
GreyHope <85015029+Grey41@users.noreply.github.com>
2021-11-30 08:18:29 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ app.use(express.static(__dirname + "/public"))
 app.get("/", (request, response) => response.render("index.html"))
 
 io.on("connection", socket => {
-    console.log("client connected")
+    console.log("Client connected")
 
     socket.on("message", data => {
         io.emit("input", data)
     })
 })
 
-server.listen(process.env.PORT || 5000, () => console.log("server listening"))
+server.listen(process.env.PORT || 5000, () => console.log("Server listening..."))
